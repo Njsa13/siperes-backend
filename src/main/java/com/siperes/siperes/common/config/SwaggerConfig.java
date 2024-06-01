f9a4016ccf373ce9f032fe5926f07c358594b16d
Njsa13 <najib.sauqi@students.amikom.ac.id>
2024-06-01 08:37:58 +0700

    Update server name

--- src/main/java/com/siperes/siperes/common/config/SwaggerConfig.java
+++ src/main/java/com/siperes/siperes/common/config/SwaggerConfig.java
@@ -6,6 +6,7 @@ import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
 
 @SecurityScheme(
         name = "bearerAuth",
@@ -15,13 +16,13 @@ import io.swagger.v3.oas.annotations.security.SecurityScheme;
         bearerFormat = "JWT",
         in = SecuritySchemeIn.HEADER
 )
-
 @OpenAPIDefinition(
         info = @Info(
                 title = "Siperes",
                 version = "v1.0.0",
                 description = "Swagger UI for Siperes"
         ),
+        servers = @Server(url = "https://siperes-tghylc3lla-as.a.run.app"),
         security = {
                 @SecurityRequirement(
                         name = "bearerAuth"
